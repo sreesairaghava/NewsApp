dcb0ad70bafe5e974b4ffab011b6f44fd978dd89
Sree Sai Raghava <raghava.rym@gmail.com>
2021-05-13 18:59:46 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Sree Sai Raghava Dandu on 13/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
